4091f3892efe9aaddc67104a760f84adcef1a26f
sahilbharne <sahil.20243246@mnnit.ac.in>
2025-07-02 16:43:24 +0530

    fix: remove deprecated options

--- Lib/mongodb.js
+++ Lib/mongodb.js
@@ -1,26 +1,26 @@
-import { MongoClient } from 'mongodb'
+import { MongoClient } from 'mongodb';
 
-const uri = process.env.MONGODB_URI
-const options = { 
-  useNewUrlParser: true,
-}
+const uri = process.env.MONGODB_URI;
+const options = {}; // no need for useNewUrlParser
 
-let client
-let clientPromise
+let client;
+let clientPromise;
 
 if (!process.env.MONGODB_URI) {
-  throw new Error('Add Mongo URI to .env.local')
+  throw new Error('Please add your Mongo URI to .env or Vercel environment variables');
 }
 
-if (process.env.NODE_ENV === 'development') { 
+if (process.env.NODE_ENV === 'development') {
+  // In dev, use a global variable so hot reload doesn't create new client
   if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options)
-    global._mongoClientPromise = client.connect()
+    client = new MongoClient(uri, options);
+    global._mongoClientPromise = client.connect();
   }
-  clientPromise = global._mongoClientPromise
+  clientPromise = global._mongoClientPromise;
 } else {
-  client = new MongoClient(uri, options)
-  clientPromise = client.connect()
+  // In production, always create a new client
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
 }
 
-export default clientPromise
+export default clientPromise;
